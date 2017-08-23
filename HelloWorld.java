dfa05c5ef2d36adfe82169a60d425d6dcdbb0364
Eric Robinson <megadeezl@comcast.net>
2017-08-23 19:36:37 -0400

    Adding first files

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int days;
+        double hours;
+        String title;
+
+        System.out.println("What is your Name");
+      //  String name = in.nextLine();
+     //   System.out.println("Hello " + name);
+
+    }
+}
